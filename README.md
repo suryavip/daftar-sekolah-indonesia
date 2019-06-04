9b5b9a6e7db8d5d6c4cf55d123741c06fd9531bc
Sudhanta Suryaputra <suryabejibun@gmail.com>
2019-06-04 20:31:04 +0700

    now save to mysql

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # daftar-sekolah-indonesia
-Ekstrak data sekolah-sekolah di Indonesia dari http://sekolah.data.kemdikbud.go.id/ dengan dokumentasi dan index  
+Ekstrak data sekolah-sekolah di Indonesia dari http://sekolah.data.kemdikbud.go.id/  
 Format URL, query dan return dari https://data.go.id/dataset/sekolah
--- extractor.py
+++ extractor.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import mysql.connector
 
 # mengumpulkan areaIDs (kode_wilayah) dari http://jendela.data.kemdikbud.go.id/api/index.php/cwilayah/wilayahKabGet
 
@@ -15,6 +16,13 @@ areaIDs = [area['kode_wilayah'].strip() for area in areas]
 
 # mengumpulkan detail sekolah dari http://jendela.data.kemdikbud.go.id/api/index.php/Csekolah/detailSekolahGET?mst_kode_wilayah=XXXXXX
 
+db = mysql.connector.connect(
+    user='root',
+    password='',
+    database='daftar-sekolah'
+)
+cursor = db.cursor()
+
 done = 0
 for areaID in areaIDs:
     r = requests.get('http://jendela.data.kemdikbud.go.id/api/index.php/Csekolah/detailSekolahGET?mst_kode_wilayah={}'.format(areaID))
@@ -22,11 +30,32 @@ for areaID in areaIDs:
         raise Exception('Gagal mengumpulkan data sekolah untuk kode_wilayah {} ({})'.format(areaID, r.status_code))
 
     schools = r.json()['data']
+    columns = schools[0].keys()
 
-    result = {}
+    if done == 0:
+        # buat table
+        cols = ['{} VARCHAR(255) NOT NULL'.format(c) for c in columns]
+        cols = ','.join(cols)
+        query = 'CREATE TABLE IF NOT EXISTS sekolah ({})'.format(cols)
+        cursor.execute((query))
+
+    cols = ','.join(columns)
+
+    val = []
+    q = []
+    for s in schools:
+        val += [d for d in s]
+        tq = ['%' for d in s]
+        q.append('({})'.format(','.join(tq)))
+    q = ','.join(q)
+
+    query = 'INSERT INTO sekolah ({}) VALUES {}'.format(cols, q)
+    cursor.execute((query), val)
+
+    '''result = {}
     result[areaID] = schools
     with open('result/{}.json'.format(areaID), 'w') as outfile:
-        json.dump(result, outfile)
+        json.dump(result, outfile)'''
 
     done += 1
     print('progress: {}/{}'.format(done, len(areaIDs)))
